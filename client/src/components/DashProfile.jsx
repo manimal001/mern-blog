6c9156eb41e9ef9b28caacbed8f71b576b9e8048
rajeeb rathour <rajeebrathour@gmail.com>
2025-03-17 13:04:57 +0545

    complete imageupload functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,16 +1,96 @@
-import { Button, TextInput } from 'flowbite-react';
-import React from 'react';
+import { Alert, Button, TextInput } from 'flowbite-react';
+import React, { useEffect, useRef, useState } from 'react';
 import {useSelector} from 'react-redux';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firebase';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 export default function DashProfile() {
-    const {currentUser} = useSelector(state => state.user)
+    const { currentUser } = useSelector((state) => state.user);
+    const [imageFile, setImageFile] = useState(null);
+    const [imageFileUrl, setImageFileUrl] = useState(null);
+    const [ imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+    const [ imageFileUploadError, setImageFileUploadError] = useState(null);
+    const filePickerRef = useRef();
+    const handleImageChange = (e) => {
+       const file = e.target.files[0];
+        if (file) {
+          setImageFile(file);
+          setImageFileUrl(URL.createObjectURL(file));
+        }
+    };
+    useEffect(() => {
+      if (imageFile) {
+        uploadImage();
+      }
+    }, [imageFile]);
+
+    const uploadImage = async () => {
+      //service firebase.storage {
+    //   match /b/{bucket}/o {
+    //     match /{allPaths=**}{
+    //     allow read;
+    //     allow write: if
+    //     request.resource.size < 2 * 1024 * 1024 &&
+    //     request.resource.contentType.matches('image/.*')
+    //   }
+    //  }
+       setImageFileUploadError(null);
+       const storage = getStorage(app);
+       const fileName = new Date().getTime() + imageFile.name;
+       const storageRef = ref(storage, fileName);
+       const uploadTask = uploadBytesResumable(storageRef, imageFile);
+        uploadTask.on('state_changed', (snapshot) => {
+           const progress = 
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            setImageFileUploadProgress(progress.toFixed(0));
+        },
+         (error) => {
+            setImageFileUploadError('Could not upload image (File must be less than 2MB)' 
+              + error.message);    
+            setImageFileUploadProgress(null);
+            setImageFile(null);
+            setImageFileUrl(null);
+         },
+         () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              setImageFileUrl(downloadURL);
+            });
+          }
+        );
+      };  
   return (
     <div className='w-full max-w-lg p-3 mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
-         <div className='self-center w-32 h-32 overflow-hidden rounded-full shadow-md cursor-pointer'>
-         <img  src={currentUser.profilePicture} alt='user' className='object-cover w-full h-full border-[lightgray] border-8 rounded-full'/>
+         <input type='file' accept='image/*' onChange={handleImageChange} ref={filePickerRef} hidden/>
+         <div className='relative self-center w-32 h-32 overflow-hidden rounded-full shadow-md cursor-pointer' onClick={() => filePickerRef.current.click()}
+          >
+            {imageFileUploadProgress && (
+               <CircularProgressbar
+                 value={imageFileUploadProgress || 0}
+                 text={`${imageFileUploadProgress}%`}
+                  strokeWidth={5}
+                  styles={{ 
+                    root: {
+                      width: '100%',
+                      height: '100%',
+                      position: 'absolute',
+                      top: 0,
+                      left: 0,
+                    },
+                    path: {
+                      stroke: 'rgba(62, 152, 199,  ${imageFileUploadProgress / 100})',
+                    },
+                   }}
+                 
+                 />
+            )}
+         <img src={imageFileUrl || currentUser.profilePicture} alt='user' className={`object-cover w-full h-full border-[lightgray] border-8 rounded-full ${imageFileUploadProgress && imageFileUploadProgress < 100 && 'opacity-60' }`}/>
          </div> 
+          { imageFileUploadError &&  <Alert color='failure'>{imageFileUploadError} </Alert>}
+          
           <TextInput type='text' id='username' placeholder='username' 
            defaultValue={currentUser.username} />
            <TextInput type='text' id='email' placeholder='email' 
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,39 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: "mern-blog-3ac45.firebaseapp.com",
+//   projectId: "mern-blog-3ac45",
+//   storageBucket: "mern-blog-3ac45.firebasestorage.app",
+//   messagingSenderId: "407189136061",
+//   appId: "1:407189136061:web:6a89ab152d3c2b240b4dd4"
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-blog-3ac45.firebaseapp.com",
-  projectId: "mern-blog-3ac45",
-  storageBucket: "mern-blog-3ac45.firebasestorage.app",
-  messagingSenderId: "407189136061",
-  appId: "1:407189136061:web:6a89ab152d3c2b240b4dd4"
+  authDomain: "rapid-project-f0780.firebaseapp.com",
+  projectId: "rapid-project-f0780",
+  storageBucket: "rapid-project-f0780.appspot.com",
+  messagingSenderId: "332862650724",
+  appId: "1:332862650724:web:c8026398722c037b95e271",
+  measurementId: "G-GG1FM7H507"
 };
 
 // Initialize Firebase
