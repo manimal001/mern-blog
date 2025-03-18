13b2ef06826f2257dd5a5797908668d80edb77b0
rajeeb rathour <rajeebrathour@gmail.com>
2025-03-18 18:41:04 +0545

    complete upload image functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,10 +1,49 @@
-import { Button, FileInput, Select, TextInput } from 'flowbite-react';
-import React from 'react';
+import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react';
+import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
+import { app } from '../firebase';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 export default function CreatePost() {
+    const [file, setFile] = useState(null);
+    const [imageUploadProgress, setImageUploadProgress] = useState(null);
+    const [imageUploadError, setImageUploadError] = useState(null);
+    const [formData, setFormData] = useState({});
+    const handleUploadImage = async () => {
+        try {
+            if (!file){
+                setImageUploadError('Please select an image');
+                return;
+            }
+            setImageUploadError(null);
+            const storage = getStorage(app);
+            const fileName = new Date().getTime() + '-' +  file.name;
+            const storageRef = ref(storage, fileName);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+            uploadTask.on('state_changed', (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                setImageUploadProgress(progress.toFixed(0));
+            }, (error) => {
+                setImageUploadError('Image upload failed'+ error.message);
+                setImageUploadProgress(null);
+            }, 
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                setImageUploadProgress(null);
+                setFormData({...formData, image: downloadURL });     
+            });
+        }
+       );
+            
+        } catch (error) {
+            setImageUploadError('Image upload failed');
+            setImageUploadProgress(null);
+            console.log(error);
+        }
+    }
   return (
     <div className='max-w-3xl min-h-screen p-3 mx-auto'>
         <h1 className='text-3xl font-semibold text-center my-7'>Create a Post</h1>
@@ -19,9 +58,30 @@ export default function CreatePost() {
             </Select>
         </div>
          <div className='flex items-center justify-between gap-4 p-4 border-4 border-teal-500 border-dotted'>
-             <FileInput type='file' accept='image/*' />
-              <Button type='button' gradientDuoTone='purpleToBlue' size='sm' outline>Upload image</Button>
-        </div> 
+             <FileInput type='file' accept='image/*' onChange={(e) => setFile(e.target.files[0])} />
+              <Button 
+              type='button' 
+              gradientDuoTone='purpleToBlue'
+              size='sm' 
+              outline 
+              onClick={handleUploadImage}
+              disabled={imageUploadProgress}
+              >
+               {
+                  imageUploadProgress? (
+                  <div className='w-16 h-16'>
+                    <CircularProgressbar value={imageUploadProgress} text= {`${imageUploadProgress || 0}%`} />
+                  </div>
+               ) : (
+                  'Upload Image'
+               )      
+               } 
+                </Button>
+           </div> 
+            {imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
+            {formData.image && (
+                <img src={formData.image} alt='upload' className='object-cover w-full h-64' />
+            )}
             <ReactQuill theme='snow' placeholder='Write Something....' className='mb-12 h-72' required />
             <Button type='submit' gradientDuoTone='purpleToPink'>Create Post</Button> 
         </form>
