b404b73ea6b97d58ee3b030d020f545508ecc601
rajeeb rathour <rajeebrathour@gmail.com>
2025-03-20 18:32:43 +0545

    add delete user functionality

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -50,7 +50,24 @@ export default function DashUsers() {
    };
    
     const handleDeleteUser = async () => {
-      
+      try {
+        const res = await fetch(`/api/user/delete/${userIdToDelete}`,{
+          method: 'DELETE',
+          headers: {
+            'Authorization': `Bearer ${currentUser.token}`, // Add token here
+            'Content-Type': 'application/json',
+          },
+        });
+        const data = await res.json();
+        if (!res.ok){
+          setUsers((prev) => prev.filter((user) => user._id !== userIdToDelete));
+          setShowModal(false);
+        }else{
+          console.log(data.message);
+        }
+      } catch (error) {
+         console.log(error.message);
+      }
     }
    
 
