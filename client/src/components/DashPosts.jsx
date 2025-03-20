1fc7c698bf4d80c19a0ebe3fc792846cad654615
rajeeb rathour <rajeebrathour@gmail.com>
2025-03-20 12:21:23 +0545

    complete deletepost functionality

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector} from 'react-redux';
-import { Table, TableHead } from 'flowbite-react';
+import { Modal, Button, Table, TableHead } from 'flowbite-react';
 import { Link } from 'react-router-dom';
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
 
 export default function DashPosts() {
    const { currentUser } = useSelector((state) => state.user);
    const [ userPosts, setUserPosts] = useState([]);
    const [showMore, setShowMore] = useState(true);
+   const [showModal, setShowModal] = useState(false);
+   const [postIdToDelete, setPostIdToDelete] = useState('');
   //  console.log(userPosts);
    useEffect(() => {
       const fetchPosts = async () => {
@@ -45,6 +48,32 @@ export default function DashPosts() {
        console.log(error.message);
      }
    }
+   
+    const handleDeletePost = async () => {
+      setShowModal(false);
+      console.log('Token:', currentUser.token);
+      try {
+        const res = await fetch(`/api/post/deletepost/${postIdToDelete}/${currentUser._id}`, {
+           method: 'DELETE',
+           headers: {
+            'Authorization': `Bearer ${currentUser.token}`, // Add token here
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+       const data = await res.json();
+       if (!res.ok) {
+        console.log('Error:', data.message);
+       }else {
+        setUserPosts((prev) => 
+           prev.filter((post) => post._id  !== postIdToDelete)
+       );
+       }   
+      } catch (error) {
+         console.log('Error:', error.message);
+      }
+    }
+   
 
   return (
     <div className='p-3 overflow-x-scroll table-auto md:mx-auto scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
@@ -77,7 +106,11 @@ export default function DashPosts() {
                     </Table.Cell>
                   <Table.Cell>{post.category}</Table.Cell>
                   <Table.Cell>
-                    <span className='font-medium text-red-500 cursor-pointer hover:underline'>
+                    <span onClick={() => {
+                      setShowModal(true);
+                      setPostIdToDelete(post._id);
+                    }}
+                    className='font-medium text-red-500 cursor-pointer hover:underline'>
                      Delete
                     </span>
                   </Table.Cell>
@@ -104,6 +137,29 @@ export default function DashPosts() {
       ):(
         <p>You have no posts yet!</p>
       )}
+      
+      <Modal show={showModal} onClose={() => setShowModal(false)}
+          popup
+          size='md'
+          >
+          <Modal.Header/> 
+          <Modal.Body>
+            <div className='text-center'>
+              <HiOutlineExclamationCircle className='h=14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto'/>
+              <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>Are you sure you want to delete your post?</h3>
+              <div className='flex justify-center gap-4'>
+                <Button color='failure' 
+                 onClick={handleDeletePost}>
+                  Yes, I'm sure
+                </Button>
+                <Button color='gray' 
+                  onClick={() => setShowModal(false)}>
+                  No,cancel
+                </Button>
+              </div>
+            </div>
+          </Modal.Body> 
+        </Modal>
     </div>
   )
 };
