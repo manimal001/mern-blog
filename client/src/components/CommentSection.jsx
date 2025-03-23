84abd9ba5cb5c7a1805f6c19894264697b99f558
rajeeb rathour <rajeebrathour@gmail.com>
2025-03-23 15:24:05 +0545

    add likes functionality to comment component

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { Alert, Button, Textarea } from 'flowbite-react';
 import Comment from './Comment';
@@ -9,6 +9,7 @@ export default function CommentSection({postId}) {
     const [comment, setComment] = useState('');
     const [ commentError, setCommentError] = useState(null);
     const [comments, setComments] = useState([]);
+    const navigate = useNavigate();
     const handleSubmit = async (e) => {
         e.preventDefault();
         if (comment.length > 200){
@@ -49,6 +50,36 @@ export default function CommentSection({postId}) {
     }
    getComments();
   }, [postId]);
+
+  const handleLike =  async (commentId) => {
+     try {
+        if (!currentUser) {
+            navigate('/sign-in');
+            return;
+        }
+        const res = await fetch(`/api/comment/likeComment/${commentId}`,{
+            method: 'PUT',
+        });
+        if (res.ok){
+            const data = await res.json();
+            setComments(
+                comments.map((comment) => 
+                comment._id === commentId 
+                ? {
+                    ...comment,
+                    likes: data.likes,
+                    numberOfLikes: data.likes.length,
+                } : comment
+            )
+        );
+        
+     }
+        
+        
+     } catch (error) {
+        console.log(error.message);
+     }
+  };
   
   return (
     <div className='w-full max-w-2xl p-3 mx-auto'>
@@ -99,13 +130,13 @@ export default function CommentSection({postId}) {
             <div className="flex items-center gap-1 my-5 text-sm">
                 <p>Comments</p>
                 <div className='px-2 py-1 border-gray-400 rounded-sm'>
-                   <p> {comments.length} </p>
+                   <p className='p-1 border border-gray-500 '> {comments.length} </p>
                 </div>
             </div>
             {
                 comments.map(comment => (
                     
-                  <Comment key={comment._id} comment={comment} />
+                  <Comment key={comment._id} comment={comment} onLike={handleLike} />
                 ))
             }
             
