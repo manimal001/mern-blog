141f3687e6658ffb7fe2b29a4fceb41d4fcd6ae1
rajeeb rathour <rajeebrathour@gmail.com>
2025-03-26 19:42:22 +0545

    complete about and projects page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-       About
+    <div className='flex items-center justify-center min-h-screen'>
+       <div>
+        <div className='max-w-2xl p-3 mx-auto text-center'>
+            <h1 className='text-3xl font-semibold font py-7'>Rajeeb's Blog</h1>
+           <div className='flex flex-col gap-6 text-gray-500 text-md'>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+              <p>
+                 It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              </p>
+              <p>
+                 Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.  Richard McClure, a professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, "consectetuer," from an old 16th-century book, The Adventures of Huckleberry Finn, and is remembered today for its role in literature.
+              </p>
+          </div>
+        </div>
+       </div>
     </div>
   )
 }
--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import CallToAction from '../components/CallToAction';
 
 export default function Project() {
   return (
-    <div>
-      project
+    <div className='flex flex-col items-center justify-center max-w-2xl min-h-screen gap-6 p-3 mx-auto'>
+       <h1 className='text-3xl font-semibold '> Projects</h1>
+       <p>Build fun and engaging projects.</p>
+       <CallToAction />
     </div>
   )
 }
